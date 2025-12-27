17ff9e112f79c0c33b15eee91a794611e09e08ff
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2025-12-28 01:50:31 +0300

    init

---
+++ src/main/java/dev/dead/springjpajdbctemplate/SpringJpaJdbcTemplateApplication.java
@@ -0,0 +1,13 @@
+package dev.dead.springjpajdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaJdbcTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaJdbcTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/dead/springjpajdbctemplate/SpringJpaJdbcTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dead.springjpajdbctemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaJdbcTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
